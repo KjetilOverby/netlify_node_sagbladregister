c57d5eaab4ae35099aec5656696724718a00451a
Kjetil Øverby <kjetiloverby@imac.home>
2020-12-27 23:00:32 +0100

    mongodb

---
+++ models/Poster.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const PosterSchema = new mongoose.Schema({
+  startRings: [
+    {
+      input: {
+        type: Number,
+      },
+    },
+  ],
+  rawInput: [
+    {
+      input: {
+        type: Number,
+      },
+      ring: {
+        type: Number,
+      },
+      shims: {
+        type: Number,
+      },
+      shims2: {
+        type: Number,
+      },
+      shims3: {
+        type: Number,
+      },
+    },
+  ],
+  endRings: [
+    {
+      input: {
+        type: Number,
+      },
+    },
+  ],
+  blades: {
+    bladStamme: {
+      type: Number,
+    },
+  },
+  header: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.models.Posts || mongoose.model('Posts', PosterSchema);
--- src/api.js
+++ src/api.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const Posts = require('../models/Poster');
+require('./mongoose');
 
 const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    hello: 'Hi',
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+
+  Posts.find({})
+    .then((Posts) => {
+      res.send(Posts);
+    })
+    .catch((e) => {});
+  //   res.json({
+  //     Hello: 'world',
+  //   });
 });
 
 app.use('/.netlify/functions/api', router);
